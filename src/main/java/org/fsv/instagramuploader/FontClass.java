8479361e52a8fa39751f4b997329053eecc32acd
pkoeppel <pkoeppel@gk-software.com>
2025-09-18 23:17:08 +0200

    update Fonts

--- src/main/java/org/fsv/instagramuploader/FontClass.java
+++ src/main/java/org/fsv/instagramuploader/FontClass.java
@@ -3,12 +3,12 @@ package org.fsv.instagramuploader;
 import java.awt.*;
 
 public class FontClass {
- public static final Font scorer = new Font("Calibri", Font.PLAIN, 26);
- public static final Font clubMen = new Font("Calibri", Font.BOLD, 36);
- public static final Font dateTimeMen = new Font("Calibri", Font.BOLD, 40);
- public static final Font clubMenResult = new Font("Arial", Font.BOLD, 26);
- public static final Font resultMen = new Font("Arial", Font.BOLD, 123);
- public static final Font headMen = new Font("Arial", Font.BOLD, 175);
+ public static final Font scorer = new Font("Calisto MT", Font.PLAIN, 26);
+ public static final Font clubMenResult = new Font("Calisto MT", Font.BOLD, 26);
+ public static final Font clubMen = new Font("Calisto MT", Font.BOLD, 36);
+ public static final Font dateTimeMen = new Font("Calisto MT", Font.BOLD, 40);
+ public static final Font resultMen = new Font("Calisto MT", Font.BOLD, 123);
+ public static final Font headMen = new Font("Calisto MT", Font.BOLD, 175);
  public static final Font headYouth1 = new Font("Comic Sans MS", Font.BOLD, 75);
  public static final Font teamYouth = new Font("Comic Sans MS", Font.BOLD, 35);
  public static final Font simpleYouth = new Font("Comic Sans MS", Font.PLAIN, 22);
